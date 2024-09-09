5173054f66e3653885577b75195f1941c931ddcf
zapzxje <zapxje.dev@gmail.com>
2024-09-09 18:10:35 +0700

    new

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    var a = 2;
-    var b = 5;
-    var c = a + b;
     return res.send('Hello World!');
 })
 
